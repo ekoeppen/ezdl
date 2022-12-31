0ddff0db3e7134e01bb885a7f4b15c6d6acd2c4f
Eckhart Köppen <eck@40hz.org>
2022-12-31 18:40:04 +0200

    Start MSP430 ports

--- src/msp430/boards/exp_msp430g2/board.zig
+++ src/msp430/boards/exp_msp430g2/board.zig
@@ -2,14 +2,10 @@ pub const mcu = @import("ezdl").msp430.mcus.msp430g2x53;
 pub const svd = @import("ezdl").msp430.svd.msp430g2x53;
 
 pub const led = mcu.Gpio(svd.P1, 0, .{ .output = .{} });
+pub const led2 = mcu.Gpio(svd.P1, 6, .{ .output = .{} });
 
 pub fn init() void {
     svd.WDT.WDTCTL.write(.{ .WDTPW = 0x5a, .WDTHOLD = 1 });
-    svd.P1.DIR.write(.{ .PIN0 = 1, .PIN6 = 1 });
-    svd.P1.OUT.modify(.{
-        .PIN1 = 1,
-    });
-    svd.P1.OUT.modify(.{
-        .PIN6 = 1,
-    });
+    led.init();
+    led2.init();
 }
--- src/msp430/peripherals/gpio.zig
+++ src/msp430/peripherals/gpio.zig
@@ -1,39 +1,48 @@
 const PullMode = enum { none, up, down };
 const OutputMode = enum { push_pull, open_drain };
 
-const PinMode = enum { input, output, alternate, analog };
+const PinMode = enum { input, output, alternate };
 const Config = union(PinMode) {
     input: struct { pull: PullMode = .none },
     output: struct { pull: PullMode = .none, mode: OutputMode = .push_pull },
     alternate: struct { pull: PullMode = .none, mode: OutputMode = .push_pull, function: u4 = 0 },
-    analog,
 };
 
-pub fn Gpio(comptime Periph: type, comptime pin: u8, comptime config: Config) type {
-    const pinBit: u3 = 1 << pin;
+fn modify(comptime reg: anytype, comptime offset: u3, comptime width: u1, value: u8) void {
+    const mask: u8 = 0xff ^ (((1 << width) - 1) << offset);
+    const bits: u8 = value << offset;
+    reg.write_raw((reg.read_raw() & mask) | bits);
+}
+
+pub fn Gpio(comptime gpio: type, comptime pin: u8, comptime config: Config) type {
+    const pinBit: u8 = 1 << pin;
     return struct {
         pub fn init() void {
             setConfig(config);
         }
 
         pub fn setConfig(c: Config) void {
-            _ = c;
+            switch (c) {
+                .input => modify(gpio.DIR, pin, 1, 0),
+                .output => modify(gpio.DIR, pin, 1, 1),
+                .alternate => {},
+            }
         }
 
         pub fn isSet() bool {
-            return if (Periph.IN.read_raw() & pinBit) true else false;
+            return if (gpio.IN.read_raw() & pinBit) true else false;
         }
 
         pub fn set() void {
-            Periph.OUT.write_raw(pinBit);
+            modify(gpio.OUT, pin, 1, 1);
         }
 
         pub fn clear() void {
-            Periph.OUT.write_raw(Periph.OUT.read_raw() & ~pinBit);
+            modify(gpio.OUT, pin, 1, 0);
         }
 
         pub fn toggle() void {
-            Periph.OUT.write_raw(Periph.OUT.read_raw() ^ pinBit);
+            gpio.OUT.write_raw(gpio.OUT.read_raw() ^ pinBit);
         }
     };
 }
