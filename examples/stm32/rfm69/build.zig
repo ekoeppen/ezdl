e82deb5a22e54186ebd8de54ab9aab0e6091b39f
Eckhart Köppen <eck@40hz.org>
2022-12-31 08:44:31 +0200

    Move RFM69 board to examples

--- examples/stm32/rfm69/build.zig
+++ examples/stm32/rfm69/build.zig
@@ -2,15 +2,12 @@ const std = @import("std");
 const ezdl = @import("ezdl/src/ezdl.zig");
 
 pub fn build(b: *std.build.Builder) anyerror!void {
-    const board_settings = try ezdl.readBoardSettings(
-        b,
-        "ezdl/src/stm32/boards/rfm69_modem/board.json",
-    );
+    const board_settings = try ezdl.readBoardSettings(b, "board.json");
     const exe = try ezdl.stm32.addExecutable(
         b,
         "rfm69.elf",
         "main.zig",
-        "ezdl/src/stm32/boards/rfm69_modem/board.zig",
+        "board.zig",
         &board_settings,
     );
     exe.install();
