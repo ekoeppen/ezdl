af03930708052b4dcb660ddaf29eec5f67458b18
Eckhart Köppen <eck@40hz.org>
2023-01-12 18:52:20 +0200

    Use addrspace for startup init

--- src/avr/startup.zig
+++ src/avr/startup.zig
@@ -1,22 +1,15 @@
-extern const _data_loadaddr: u16;
+extern const _data_loadaddr: u16 addrspace(.flash);
 extern var _data: u16;
 extern const _edata: u16;
 extern var _bss: u16;
 extern const _ebss: u16;
 extern const _stack: u16;
 
-inline fn lpm(ptr: *const u8) u8 {
-    return asm ("lpm %[ret], %[pm]"
-        : [ret] "=a" (-> u8),
-        : [pm] "z" (ptr),
-    );
-}
-
 export fn resetHandler() callconv(.Naked) void {
-    const data_loadaddr = @ptrCast([*]const u8, &_data_loadaddr);
+    const data_loadaddr = @ptrCast([*]addrspace(.flash) const u8, &_data_loadaddr);
     const data = @ptrCast([*]u8, &_data);
     const data_size = @ptrToInt(&_edata) - @ptrToInt(&_data);
-    for (data_loadaddr[0..data_size], data[0..data_size]) |*from, *to| to.* = lpm(from);
+    for (data_loadaddr[0..data_size], data[0..data_size]) |from, *to| to.* = from;
 
     const bss = @ptrCast([*]u8, &_bss);
     const bss_size = @ptrToInt(&_ebss) - @ptrToInt(&_bss);
