ebfae8c10ad3ce39132dc6c0aa3e6996b5ae5715
Eckhart Köppen <eck@40hz.org>
2023-01-17 07:36:09 +0200

    Change region size calculation in STM32 startup

--- src/stm32/startup.zig
+++ src/stm32/startup.zig
@@ -1,22 +1,21 @@
-// const ezdl = @import("ezdl");
-// const board = @import("board");
-
-extern const _data_loadaddr: u32;
-extern var _data: u32;
-extern const _edata: u32;
-extern var _bss: u32;
-extern const _ebss: u32;
+extern const _data_loadaddr: anyopaque;
+extern var _data: anyopaque;
+extern const _edata: anyopaque;
+extern var _bss: anyopaque;
+extern const _ebss: anyopaque;
 
 extern fn main() void;
 
 export fn resetHandler() void {
     const data_loadaddr = @ptrCast([*]const u8, &_data_loadaddr);
     const data = @ptrCast([*]u8, &_data);
-    const data_size = @ptrToInt(&_edata) - @ptrToInt(&_data);
+    const data_end = @ptrCast([*]u8, &_edata);
+    const data_size = @ptrToInt(data_end) - @ptrToInt(data);
     for (data_loadaddr[0..data_size]) |d, i| data[i] = d;
 
     const bss = @ptrCast([*]u8, &_bss);
-    const bss_size = @ptrToInt(&_ebss) - @ptrToInt(&_bss);
+    const bss_end = @ptrCast([*]u8, &_ebss);
+    const bss_size = @ptrToInt(bss_end) - @ptrToInt(bss);
     for (bss[0..bss_size]) |*b| b.* = 0;
 
     asm volatile ("sev");
