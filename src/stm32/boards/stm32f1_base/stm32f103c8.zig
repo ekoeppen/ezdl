ca1bfe03bd9c6430a23c09994f4461ef5f67b35d
Eckhart Köppen <eck@40hz.org>
2023-02-05 20:28:15 +0200

    Fix STM32F103C8 memory size

--- src/stm32/boards/stm32f1_base/stm32f103c8.zig
+++ src/stm32/boards/stm32f1_base/stm32f103c8.zig
@@ -12,7 +12,7 @@ pub const config = .{
     .programmer = "jlink",
     .memory = &.{
         .{ .name = "rom", .attrs = "rx", .start = 0x08000000, .size = 65536 },
-        .{ .name = "ram", .attrs = "rwx", .start = 0x20000000, .size = 16384 },
+        .{ .name = "ram", .attrs = "rwx", .start = 0x20000000, .size = 20480 },
     },
     .board_path = std.fs.path.dirname(path()).? ++ "/board.zig",
 };
