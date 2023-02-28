8726c8c88cfed141e94934f6c2fc5b34c0996c28
Eckhart Köppen <eck@40hz.org>
2023-02-28 07:44:53 +0200

    Handle building outside of a git repo

--- src/lib/build_info.zig
+++ src/lib/build_info.zig
@@ -8,16 +8,19 @@ pub fn main() !void {
     const file = try std.fs.cwd().createFile("zig-cache/build_info.zig", .{
         .truncate = true,
     });
-    const git = try std.ChildProcess.exec(.{
-        .argv = &.{ "git", "rev-parse", "--short", "HEAD" },
-        .allocator = allocator,
-    });
+    const commit = c: {
+        const git = std.ChildProcess.exec(.{
+            .argv = &.{ "git", "rev-parse", "--short", "HEAD" },
+            .allocator = allocator,
+        }) catch break :c "<none";
+        break :c if (git.term.Exited == 0) git.stdout[0..7] else "<none>";
+    };
     var now = DateTime{};
     now.set(@intCast(u64, std.time.timestamp()));
     _ = try file.writer().print(
         \\pub const build_time = "{s}Z";
         \\pub const commit = "{s}";
         \\
-    , .{ now, git.stdout[0..7] });
+    , .{ now, commit });
     defer file.close();
 }
