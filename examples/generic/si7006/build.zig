b6c089e23b3e4b0593df6b63a9775e50852e4cb3
Eckhart Köppen <eck@40hz.org>
2023-02-28 16:55:43 +0200

    Fixup

--- examples/generic/si7006/build.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const ezdl = @import("ezdl/src/ezdl.zig");
-
-pub fn build(b: *std.build.Builder) anyerror!void {
-    const board_name = b.option([]const u8, "board", "Board") orelse "nucleo_f072rb";
-    const board = try ezdl.boardFromName(board_name);
-    const exe = try ezdl.addExecutable(b, "si7006.elf", "main.zig", &board);
-    b.installArtifact(exe);
-}
--- examples/stm32/rtc/build.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const ezdl = @import("ezdl/src/ezdl.zig");
-
-pub fn build(b: *std.build.Builder) anyerror!void {
-    const board = ezdl.stm32.rfm69_modem;
-    const exe = try ezdl.stm32.addExecutable(b, "rtc.elf", "main.zig", board);
-    exe.install();
-}
